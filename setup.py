8351ffa7d7dbb0f14d83a9b182beb8b88670322b
Ray Greenfield <rgreenfield4@student.gsu.edu>
2024-04-10 20:16:19 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return a list of requirements
+    '''
+    requirements = []
+    with open('requirements.txt') as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements ]
+
+
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='RayG',
+    author_email='rymndgreenfield@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt'),
+    
+    
+)
