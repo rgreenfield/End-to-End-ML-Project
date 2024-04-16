4ad691adcb7ef426e15dee941d094c3c02f976a0
Ray Greenfield <rgreenfield4@student.gsu.edu>
2024-04-16 10:14:35 -0400

    updated file

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def get_requirements(file_path:str)->List[str]:
 setup(
     name='mlproject',
     version='0.0.1',
-    author='RayG',
+    author='Ray G',
     author_email='rymndgreenfield@gmail.com',
     packages=find_packages(),
     install_requires=get_requirements('requirements.txt'),
